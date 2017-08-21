830c50cae0b504d1f25842ec7121e11dfc9ac6cd
Nathan Paige <npaige@oberlin.edu>
2017-08-20 21:52:09 -0700

    forgot to include 2 files

---
+++ Context.java
@@ -0,0 +1,32 @@
+import java.util.List;
+public class Context {
+
+	public VarST vars;
+	public FuncST funcs;
+
+	public VarST chain_vars;
+	public FuncST chain_funcs;
+
+	public Context (VarST v, FuncST f) {
+		vars = v;
+		funcs = f;
+		chain_vars = v;
+		chain_funcs = f;
+	}
+
+	public Context setChainClass (Klass k) {
+		Context c = new Context (vars, funcs);
+		c.chain_vars = k.ctx.vars;
+		c.chain_funcs = k.ctx.funcs;
+		return c;
+	}
+
+	public DeclTree lookupVar (String name, boolean dotchain) {
+		return dotchain ? chain_vars.lookup(name) : vars.lookup(name);
+	}
+
+	public List<DeclTree> lookupFunc (String name, boolean dotchain) {
+		return dotchain ? chain_funcs.lookup(name) : funcs.lookup(name);
+	}
+
+}
